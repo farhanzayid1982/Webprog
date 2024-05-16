9cd8bd6dc40472cb0acb4e93af2ef1183e6535b4
farhanzayid1982 <farhan.zayid@gmail.com>
2024-05-16 19:56:59 +0700

    modified:   file.php

---
+++ file.php
@@ -0,0 +1,6 @@
+<?php
+    $data = 'Ini datanya';
+    echo 'data = $data'; //Ditulis menggunakan single quote
+    echo "data = $data"; //Ditulis menggunakan double quote
+    echo `data = $data`; //Ditulis menggunakan Aphosthrope
+?>
