d39b507c52b4b3767cc72198192350447bd8ba08
farhanzayid1982 <farhan.zayid@gmail.com>
2024-05-20 15:54:49 +0700

    modified:   file.php     newfile: index.html

--- file.php
+++ file.php
@@ -1,6 +1,6 @@
 <?php
     $data = 'Ini datanya';
     echo 'data = $data'; //Ditulis menggunakan single quote
-    echo "data = $data"; //Ditulis menggunakan double quote
-    echo `data = $data`; //Ditulis menggunakan Aphosthrope
+    echo "data = $data"; //Ditulis menggunakan double quote isi data dari $data di munculkan
+    echo `data = $data`; //Ditulis menggunakan Aphosthrope, ini seharusnya tidak muncul karena error
 ?>
